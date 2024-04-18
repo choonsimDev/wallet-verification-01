bbcf1555a0a7ff684e0f4e8480868adf6ff7c339
남형석 <namhyeongseog@namhyeongseogs-MacBook-Pro.local>
2024-04-18 13:46:36 +0900

    add button for scanner

--- components/MainVerification.js
+++ components/MainVerification.js
@@ -17,41 +17,42 @@ export default function Verification() {
     const [oldSecretNumberExists, setOldSecretNumberExists] = useState(false); // null로 초기화하여 아직 확인되지 않음을 나타냅니다.
     const [handleAllPassword, setHandleAllPassword] = useState(false);
     const [newAccount, setNewAccount] = useState(false);
+    const [scannerVisible, setScannerVisible] = useState(false); // New state to manage scanner visibility
+
+    const toggleScanner = () => {
+        setScannerVisible(!scannerVisible); // Toggle the visibility state
+    };
+
 
     const getWalletAccount = (data) => {
         setWalletAccount(data);
-        // console.log("walletAccount-from-QRscanner", data);
-        // console.log("walletAccount-from-QRscanner", typeof data)
     }
     const checkOldSecretNumberExists = (exists) => {
         setOldSecretNumberExists(exists);
     }
     const AllPasswordCorrect = (isCorrect) => {
         setHandleAllPassword(isCorrect);
-        // 여기서 isCorrect가 true일 경우, 즉 모든 비밀번호가 올바를 때 원하는 로직을 실행합니다.
-        // 예: 사용자에게 성공 메시지를 표시하거나, 다음 폼으로 네비게이션 하는 등
         if (isCorrect) {
-            // 모든 비밀번호가 정확하다는 메시지 표시 또는 다른 액션 실행
             console.log("모든 비밀번호가 정확합니다. 다음 단계로 진행하세요.");
         }
     };
     const getNewAccount = (data) => {
         setNewAccount(data);
-        // console.log("newAccount", data);
-        // console.log("newAccount", typeof data)
     }
     useEffect(() => {
-
+        // This effect could be used for additional logic when handleAllPassword changes.
     }, [handleAllPassword]);
 
     return (
         <Center>
             <TitleDescription />
-            {/* Todo : 버튼으로 카메라를 열고,어카운트 입력이 완료되면, 카메라 창 닫아야함. */}
-            <Scanner
-                getWalletAccount={getWalletAccount}
-                getNewAccount={getNewAccount}>
-            </Scanner>
+            <button onClick={toggleScanner}>Toggle Camera</button> {/* Button to open/close the scanner */}
+            {scannerVisible && ( // Conditionally render the Scanner component
+                <Scanner
+                    getWalletAccount={getWalletAccount}
+                    getNewAccount={getNewAccount}>
+                </Scanner>
+            )}
 
             <StyledSecretNumberBox >
                 <div>OldSecretNumber를 차례대로 입력하셔야 새로운 비밀번호를 만들 수 있습니다.</div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Header from "@/components/Header";
-import Verification from "@/components/MainVerification";
+import MainVerification from "@/components/MainVerification";
 
 export default function Home() {
   return (
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <Verification />
+      <MainVerification />
     </>
   );
 }
